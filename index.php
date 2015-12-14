871f8bd90bbe332bae7d3c954bfc9d567bc14d72
Rich Hildred <rhildred@gmail.com>
2015-12-13 22:46:05 -0500

    Revert "updated amounts to be tasklist"
    This reverts commit 50bd01d485c028aec890dc991eeb2f6e2d89a931.

--- index.php
+++ index.php
@@ -4,18 +4,15 @@ require "vendor\autoload.php";
 
 $oApp = new \Slim\Slim(array('templates.path' => __DIR__));
 // open databaase
-$oDb = new PDO("sqlite:" . __DIR__ . "/tasks.sqlite");
+$oDb = new PDO("sqlite:" . __DIR__ . "/amounts.sqlite");
 
 $oApp->get("/", function() use($oApp){
-    $oApp->render("tasks.phtml");
+    $oApp->render("addingMachine.phtml");
 });
 
-$oApp->post("/tasks", function() use($oApp, $oDb){
+$oApp->post("/", function() use($oApp, $oDb){
     $oData = json_decode($oApp->request->getBody());
-    $oStmt = $oDb->prepare("INSERT INTO tasks(description) VALUES(:task)");
-    $oStmt->bindParam("task", $oData->currentTask);
-    $oStmt->execute();
-    echo json_encode(array("rows"=>$oStmt->rowCount()));
+    print_r($oData);
 });
 
 $oApp->run();
