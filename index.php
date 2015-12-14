f35f2cc4d4dbf9071f13361fe1f7895c71713d1c
Rich Hildred <rhildred@gmail.com>
2015-12-13 22:46:09 -0500

    Revert "got front end communicating with back end"
    This reverts commit 43e04332c6236032f9140cee3916c3c65901401a.

--- index.php
+++ index.php
@@ -3,16 +3,9 @@
 require "vendor\autoload.php";
 
 $oApp = new \Slim\Slim(array('templates.path' => __DIR__));
-// open databaase
-$oDb = new PDO("sqlite:" . __DIR__ . "/amounts.sqlite");
 
 $oApp->get("/", function() use($oApp){
     $oApp->render("addingMachine.phtml");
-});
-
-$oApp->post("/", function() use($oApp, $oDb){
-    $oData = json_decode($oApp->request->getBody());
-    print_r($oData);
 });
 
 $oApp->run();
